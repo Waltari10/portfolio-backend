3aebad668bc1370490f90ba2c4acc2d154eea2c4
Valtteri Laine <valtteri.e.laine@gmail.com>
2019-10-27 18:31:55 +0200

    Fixes crash.

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ const sendEmail = (from, to, subject, text) => {
     subject,
     text
   };
-  console.log(mailOptions)
 
   return new Promise((resolve, reject) => {
     
@@ -44,38 +43,39 @@ app.post('/comment', function (req, res) {
 
   const { name = '', email = '', message = ''} = req.body || {};
 
-  if (typeof email !== 'string' || typeof message !== 'string' || typeof name !== 'string' ) {
+  if (typeof message !== 'string' || typeof name !== 'string' ) {
 
       res.status(400);
       res.send('Please check that email, message and name are correctly entered.');
       return;
   }
 
-  const sendConfirmationEmailPromise = sendEmail(
-    process.env.EMAIL_SENDER,
-    email,
-    'Your contact email has been sent to Valtteri',
-    'Your contact email has been sent to Valtteri Laine. He will get back to you shortly.'
-  )
+  let sendConfirmationEmailPromise;
+
+  if (typeof email === 'string' && !!email) {
+    sendConfirmationEmailPromise = sendEmail(
+      process.env.EMAIL_SENDER,
+      email,
+      'Your contact email has been sent to Valtteri',
+      'Your contact email has been sent to Valtteri Laine. He will get back to you shortly.'
+    );
+  }
 
   const sendContactEmailPromise = sendEmail(
     process.env.EMAIL_SENDER, 
     process.env.EMAIL_RECEIVER, 
     name + ' contacted you via portfolio site', 
     message
-  )
+  );
 
 
   Promise.all([sendConfirmationEmailPromise, sendContactEmailPromise])
   .then(() => {
-    console.log('then')
       res.status(200);
       res.send('Email sent succesfully');
       console.log('Email sent: ' + info.response);
   })
   .catch((err) => {
-    console.log(err)
-    console.log('catch')
       res.status(500);
       res.send('Error sending email! Please send email manually to ' + process.env.EMAIL_SENDER);
   })
